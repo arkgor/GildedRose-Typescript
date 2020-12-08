d06b1220cccae98cd33eeebcb3c3d06722b51fb0
Arek Gorecki <ark.gorecki@gmail.com>
2020-12-08 18:17:36 +0100

    style: formats code with prettier

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -4,9 +4,9 @@ export class Item {
   quality: number;
 
   constructor(name, sellIn, quality) {
-      this.name = name;
-      this.sellIn = sellIn;
-      this.quality = quality;
+    this.name = name;
+    this.sellIn = sellIn;
+    this.quality = quality;
   }
 }
 
@@ -14,77 +14,79 @@ export class GildedRose {
   items: Array<Item>;
 
   constructor(items = [] as Array<Item>) {
-      this.items = items;
+    this.items = items;
   }
 
   updateQuality() {
-      for (let item of this.items) {
-          if (this.isSulfuras(item)) {
-            // don't do anything
-          } else if (this.isGeneric(item)) {
-              if (item.quality > 0) {
-                  this.decreaseItemQuality(item);
-              }
-          }
-          else {
+    for (let item of this.items) {
+      if (this.isSulfuras(item)) {
+        // don't do anything
+      } else if (this.isGeneric(item)) {
+        if (item.quality > 0) {
+          this.decreaseItemQuality(item);
+        }
+      } else {
+        if (this.isQualityLessThanMax(item)) {
+          this.increaseItemQuality(item);
+          if (this.isBackstagePass(item)) {
+            if (item.sellIn < 10) {
               if (this.isQualityLessThanMax(item)) {
-                   this.increaseItemQuality(item);
-                  if (this.isBackstagePass(item)) {
-                      if (item.sellIn < 10) {
-                          if (this.isQualityLessThanMax(item)) {
-                               this.increaseItemQuality(item);
-                          }
-                      }
-                      if (item.sellIn < 6) {
-                          if (this.isQualityLessThanMax(item)) {
-                               this.increaseItemQuality(item);
-                          }
-                      }
-                  }
+                this.increaseItemQuality(item);
               }
-          }
-
-          if (!this.isSulfuras(item)) {
-              item.sellIn = item.sellIn - 1;
-          }
-
-          if (item.sellIn < 0) {
-              if (!this.isAgedBrie(item)) {
-                  if (!this.isBackstagePass(item)) {
-                      if (item.quality > 0) {
-
-                          if (!this.isSulfuras(item)) {
-                              this.decreaseItemQuality(item);
-                          }
-                      }
-                  } else {
-                      item.quality = item.quality - item.quality
-                  }
-              } else {
-                  if (this.isQualityLessThanMax(item)) {
-                      this.increaseItemQuality(item);
-                  }
+            }
+            if (item.sellIn < 6) {
+              if (this.isQualityLessThanMax(item)) {
+                this.increaseItemQuality(item);
               }
+            }
           }
+        }
       }
 
-      return this.items;
+      if (!this.isSulfuras(item)) {
+        item.sellIn = item.sellIn - 1;
+      }
+
+      if (item.sellIn < 0) {
+        if (!this.isAgedBrie(item)) {
+          if (!this.isBackstagePass(item)) {
+            if (item.quality > 0) {
+              if (!this.isSulfuras(item)) {
+                this.decreaseItemQuality(item);
+              }
+            }
+          } else {
+            item.quality = item.quality - item.quality;
+          }
+        } else {
+          if (this.isQualityLessThanMax(item)) {
+            this.increaseItemQuality(item);
+          }
+        }
+      }
+    }
+
+    return this.items;
   }
 
   private isGeneric(item: Item) {
-    return !(this.isSulfuras(item) || this.isBackstagePass(item) || this.isAgedBrie(item))
+    return !(
+      this.isSulfuras(item) ||
+      this.isBackstagePass(item) ||
+      this.isAgedBrie(item)
+    );
   }
 
   private isSulfuras(item: Item) {
-    return item.name == 'Sulfuras, Hand of Ragnaros';
+    return item.name == "Sulfuras, Hand of Ragnaros";
   }
 
   private isBackstagePass(item: Item) {
-    return  item.name == 'Backstage passes to a TAFKAL80ETC concert';
+    return item.name == "Backstage passes to a TAFKAL80ETC concert";
   }
 
   private isAgedBrie(item: Item) {
-    return item.name == 'Aged Brie';
+    return item.name == "Aged Brie";
   }
 
   private isQualityLessThanMax(item: Item) {
